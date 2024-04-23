e304ab22bf0112443438af335b9f9cd347918bb4
al-martyn1 <amart@mail.ru>
2024-04-24 01:12:15 +0300

    Пофиксил тупые баги

--- tests/_set_pp_sample.bat
+++ tests/_set_pp_sample.bat
@@ -1,2 +1,2 @@
-set PP=.%~dp0\.\.build\GCC-73_32-QT\unknown\Debug\bin\umba-md-pp.exe
-set PP=%~dp0\..\.out\msvc2019\x86\Debug\umba-md-pp.exe
+@set PP=%~dp0\..\.build\GCC-73_32-QT\unknown\Debug\bin\umba-md-pp.exe
+@set PP=%~dp0\..\.out\msvc2019\x86\Debug\umba-md-pp.exe
