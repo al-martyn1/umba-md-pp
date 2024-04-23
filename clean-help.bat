11be8b6b5d781d7c93a1148cea4f8096f345aa66
al-martyn1 <amart@mail.ru>
2024-04-24 01:16:40 +0300

    Запилил генерацию всевозможных хелпов

---
+++ clean-help.bat
@@ -0,0 +1,4 @@
+@if exist %~dp0\umba-md-pp-help.txt   del %~dp0\umba-md-pp-help.txt
+@if exist %~dp0\umba-md-pp-help.md    del %~dp0\umba-md-pp-help.md
+@if exist %~dp0\umba-md-pp-help.bash  del %~dp0\umba-md-pp-help.bash
+@if exist %~dp0\umba-md-pp-help.clink del %~dp0\umba-md-pp-help.clink
---
+++ mk-help.bat
@@ -0,0 +1,8 @@
+@call %~dp0\tests\_setup.bat
+%PP%         --help > umba-md-pp-help.txt
+%PP% --md    --help > umba-md-pp-help.md
+%PP% --bash  --help > umba-md-pp-help.bash
+%PP% --clink --help > umba-md-pp-help.clink
+
+
+
