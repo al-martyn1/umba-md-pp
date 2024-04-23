1f9433055fc093349a0ba4ffd532fb25b63ab069
al-martyn1 <amart@mail.ru>
2024-04-23 23:48:43 +0300

    Подпилил тесты

--- tests/set_pp_sample.bat
+++ tests/set_pp_sample.bat
@@ -1,1 +1,2 @@
-set PP=..\.build\GCC-73_32-QT\unknown\Debug\bin\umba-md-pp.exe
+set PP=.%~dp0\.\.build\GCC-73_32-QT\unknown\Debug\bin\umba-md-pp.exe
+set PP=%~dp0\..\.out\msvc2019\x86\Debug\umba-md-pp.exe
