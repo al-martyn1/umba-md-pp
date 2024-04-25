6da7027cc450814cbec64448198750690686da13
Alexandr Martynov <mag@unicont.com>
2024-04-25 16:43:16 +0300

    Добавил пример бесполезной шапки файла

---
+++ tests/snippets/cpp/useless.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : eclipse_test01_cpp.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
