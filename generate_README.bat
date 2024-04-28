0ab3a11afd5e2bb0fa3a83c3150640abd4280c54
al-martyn1 <amart@mail.ru>
2024-04-28 21:58:17 +0300

    Пилим доки на MD-PP

---
+++ generate_README.bat
@@ -0,0 +1,12 @@
+@call mk-help.bat
+@call %~dp0\tests\_setup.bat
+
+@set PROCESSING_OPTIONS=--processing-options=numeric-sections,generateSectionId,generate-toc
+@set SEC_LEVELS=--numeric-sections-max-level=2 --toc-max-level=3
+@set SNIPPETS_PATHS=%~dp0\..;%~dp0\snippets
+@set INSERT_OPTIONS=--set-insert-options=filename,path,filenameLineNo,fail,trim-arround,snippet-options
+@set OPTS=--overwrite --add-examples-path=%SNIPPETS_PATHS% %PROCESSING_OPTIONS% %SEC_LEVELS% %INSERT_OPTIONS%
+@rem %PP% %OPTS% %OPTS% README.md_ README.md
+
+
+@call clean-help.bat
