3aedd1913433c0228df4fc6efd62abcb5bfb66a8
al-martyn1 <amart@mail.ru>
2024-04-28 14:55:52 +0300

    Убрал генерацию номеров разделов в четвертом тесте

--- tests/test04.bat
+++ tests/test04.bat
@@ -1,5 +1,6 @@
 @if exist %~dp0\_setup.bat @call %~dp0\_setup.bat
-@set OPTS=--processing-options=numeric-sections,generateSectionId,generate-toc --numeric-sections-max-level=2 --toc-max-level=3
+@rem numeric-sections,
+@set OPTS=--processing-options=generateSectionId,generate-toc --numeric-sections-max-level=2 --toc-max-level=3
 %PP% %CMNOPTS% %OPTS% test04.md_ test04.md
 copy /Y test04.md generated_toc_sample.md
 
