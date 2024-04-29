49ed10aeaef618ab3a710390f26c89dc6021295f
al-martyn1 <amart@mail.ru>
2024-04-29 17:55:33 +0300

    Перенёс закоментированные тесты в отдельный файл, надо будет - буду оттуда доставать, или оформлю в отдельные EXE

---
+++ src/umba-md-pp/tests_bak.cpp
@@ -0,0 +1,76 @@
+/*! \file
+    \brief Всякие тестики, которые раньше тусовались прямо в коде тулзы. Ни как не оформлены, сами по себе не запускаются
+ */
+
+
+    auto testCondition = [&](std::string cond, bool expectedRes)
+    {
+        std::cout << "Condition: " << cond << "\n";
+        try
+        {
+            bool res = isConditionTrue(appConfig.conditionVars, cond);
+            if (res==expectedRes)
+            {
+                std::cout << " + passed, res: " << (res?"true":"false") << "\n";
+            }
+            else
+            {
+                std::cout << " - failed, res: " << (res?"true":"false") << ", expected: " << (expectedRes?"true":"false") << "\n";
+            }
+        }
+        catch(...)
+        {
+            std::cout << " - unexpected error\n";
+        }
+    };
+    
+    appConfig.addConditionVar("A", "A");
+    appConfig.addConditionVar("B", "B");
+    appConfig.addConditionVar("C:C");
+    testCondition("$A==A", true);
+    testCondition("$A!=A", false);
+    testCondition("$A!=$A", false);
+    testCondition("$A!=$B", true);
+    testCondition("$C!=C", false);
+
+
+
+
+    auto testGhIdGen = [&](std::string s)
+    {
+        std::cout << s << ": [" << umba::generateIdFromText_forGitHub(s) << "]\n";
+    };
+     
+    testGhIdGen("Воскл-бакс! \"#$");
+    testGhIdGen("Прцнт-апоз%&'");
+    testGhIdGen("Опнскб-плюс()*+");
+    testGhIdGen("Зпт,");
+    testGhIdGen("Минус-");
+    testGhIdGen("Тчк.");
+    testGhIdGen("Дел-вопр/:;<=>?");
+    testGhIdGen("Соб-подч@[\\]^_");
+    testGhIdGen("Бктик`");
+    testGhIdGen("ФгСкбЛ{");
+    testGhIdGen("ФгСкбП}");
+    testGhIdGen("Ор|");
+    testGhIdGen("Тильда~");
+    testGhIdGen("English Title");
+
+
+
+
+    std::string levelStr;
+    std::string headerText;
+    splitHeaderLine("   ### Some ##", levelStr, headerText);
+
+
+
+    std::unordered_set<SnippetOptions> snippetFlagsOptions = appConfig.snippetOptions;
+    std::string snippetFile;
+    std::string snippetTag ;
+    if (!parseSnippetInsertionCommandLine(snippetFlagsOptions, "#!insert{no-lineno} path/to/sample/file.cpp#TAG", snippetFile, snippetTag))
+    {
+    }
+
+
+
--- src/umba-md-pp/umba-md-pp.cpp
+++ src/umba-md-pp/umba-md-pp.cpp
@@ -200,60 +200,6 @@ int main(int argc, char* argv[])
 
     //unsigned errCount = 0;
 
-    // auto testCondition = [&](std::string cond, bool expectedRes)
-    // {
-    //     std::cout << "Condition: " << cond << "\n";
-    //     try
-    //     {
-    //         bool res = isConditionTrue(appConfig.conditionVars, cond);
-    //         if (res==expectedRes)
-    //         {
-    //             std::cout << " + passed, res: " << (res?"true":"false") << "\n";
-    //         }
-    //         else
-    //         {
-    //             std::cout << " - failed, res: " << (res?"true":"false") << ", expected: " << (expectedRes?"true":"false") << "\n";
-    //         }
-    //     }
-    //     catch(...)
-    //     {
-    //         std::cout << " - unexpected error\n";
-    //     }
-    // };
-    
-    // appConfig.addConditionVar("A", "A");
-    // appConfig.addConditionVar("B", "B");
-    // appConfig.addConditionVar("C:C");
-    // testCondition("$A==A", true);
-    // testCondition("$A!=A", false);
-    // testCondition("$A!=$A", false);
-    // testCondition("$A!=$B", true);
-    // testCondition("$C!=C", false);
-
-    // auto testGhIdGen = [&](std::string s)
-    // {
-    //     std::cout << s << ": [" << umba::generateIdFromText_forGitHub(s) << "]\n";
-    // };
-    //  
-    // testGhIdGen("Воскл-бакс! \"#$");
-    // testGhIdGen("Прцнт-апоз%&'");
-    // testGhIdGen("Опнскб-плюс()*+");
-    // testGhIdGen("Зпт,");
-    // testGhIdGen("Минус-");
-    // testGhIdGen("Тчк.");
-    // testGhIdGen("Дел-вопр/:;<=>?");
-    // testGhIdGen("Соб-подч@[\\]^_");
-    // testGhIdGen("Бктик`");
-    // testGhIdGen("ФгСкбЛ{");
-    // testGhIdGen("ФгСкбП}");
-    // testGhIdGen("Ор|");
-    // testGhIdGen("Тильда~");
-    // testGhIdGen("English Title");
-     
-
-    // std::string levelStr;
-    // std::string headerText;
-    // splitHeaderLine("   ### Some ##", levelStr, headerText);
 
     std::string inputFileText;
     //if (!umba::filesys::readFile(inputFilename, inputFileText))
@@ -269,27 +215,18 @@ int main(int argc, char* argv[])
 
     UMBA_USED(lineNo);
 
+
+
+
+
     //std::string 
     curFile = inputFilename; // = fileName;
     //unsigned lineNo = 0;
 
-    // std::unordered_set<SnippetOptions> snippetFlagsOptions = appConfig.snippetOptions;
-    // std::string snippetFile;
-    // std::string snippetTag ;
-    // if (!parseSnippetInsertionCommandLine(snippetFlagsOptions, "#!insert{no-lineno} path/to/sample/file.cpp#TAG", snippetFile, snippetTag))
-    // {
-    // }
-
-    //inputFileText = AppConfig::autoEncodeToUtf(inputFileText);
 
     appConfig.checkAdjustDocNumericLevels();
 
     std::string resText = processMdFile(appConfig, inputFileText, inputFilename);
-
-    //TODO: !!! если файл существует, его надо обнулить
-
-    //std::string resultText = marty_cpp::converLfToOutputFormat(inputFileText, appConfig.outputLinefeed);
-
 
 
     try
