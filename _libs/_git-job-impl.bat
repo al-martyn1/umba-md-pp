f3011323fa6bb099ae53b646062c1d105162bfb2
al-martyn1 <amart@mail.ru>
2024-05-04 00:45:49 +0300

    Присунул либу marty_tr

--- _libs/_git-job-impl.bat
+++ _libs/_git-job-impl.bat
@@ -1,5 +1,6 @@
 @call :DO_GIT_JOB %1 %~dp0\encoding
 @call :DO_GIT_JOB %1 %~dp0\marty_cpp
+@call :DO_GIT_JOB %1 %~dp0\marty_tr
 @call :DO_GIT_JOB %1 %~dp0\marty_yaml_toml_json
 @call :DO_GIT_JOB %1 %~dp0\nlohmann
 @call :DO_GIT_JOB %1 %~dp0\sfmt
