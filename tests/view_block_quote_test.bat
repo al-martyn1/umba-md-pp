819924af047c674ffffe121b980b2b6ffeb8b674
Alexandr Martynov <mag@unicont.com>
2024-05-07 11:45:45 +0300

    Добавил запускалку для просмотра block quote теста

---
+++ tests/view_block_quote_test.bat
@@ -0,0 +1,8 @@
+@if exist %~dp0\_setup.bat @call %~dp0\_setup.bat
+@set OPTS=--processing-options=no-numeric-sections,generateSectionId,generate-toc --numeric-sections-max-level=2 --toc-max-level=3
+%PPVIEWCON% %CMNOPTS% %OPTS% block_quote_test.md
+
+
+
+
+
