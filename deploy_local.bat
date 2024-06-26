c7819f2e37db699b6c47ef8cb922265b9a619394
al-martyn1 <amart@mail.ru>
2024-06-27 00:43:44 +0300

    Обновил батники обновления конфиша для отладки и локального деплоя

--- deploy_local.bat
+++ deploy_local.bat
@@ -9,14 +9,15 @@
 
 :UMBA_TOOLS_VAR_IS_SET
 
+@call %~dp0\update_md-pp-view-conf-options.bat
 
 @if not exist %UMBA_TOOLS%\bin    mkdir %UMBA_TOOLS%\bin
 @if not exist %UMBA_TOOLS%\conf   mkdir %UMBA_TOOLS%\conf
 
-@copy /Y .out\msvc2019\x64\Release\umba-md-pp.exe         %UMBA_TOOLS%\bin\
-@copy /Y .out\msvc2019\x64\Release\umba-md-pp-view.exe    %UMBA_TOOLS%\bin\
+@copy /Y %~dp0\.out\msvc2019\x64\Release\umba-md-pp.exe         %UMBA_TOOLS%\bin\
+@copy /Y %~dp0\.out\msvc2019\x64\Release\umba-md-pp-view.exe    %UMBA_TOOLS%\bin\
 
-@xcopy /Y /S /E /I /F /R _distr_conf\conf\*               %UMBA_TOOLS%\conf
+@xcopy /Y /S /E /I /F /R %~dp0\_distr_conf\conf\*               %UMBA_TOOLS%\conf
 
 %UMBA_TOOLS%\bin\umba-md-pp-view.exe --register-view-handler
 
