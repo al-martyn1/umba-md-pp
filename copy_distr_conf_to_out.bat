6be1b576186b85cb91baeee3cb1c06203341f96b
al-martyn1 <amart@mail.ru>
2024-06-27 00:29:25 +0300

    Поправил/добавил файлы для деплоя бинарника в локальную установку, и для копирования конфигов в .out для отладки

--- copy_distr_conf_to_out.bat
+++ copy_distr_conf_to_out.bat
@@ -1,4 +1,17 @@
-@call tests\_set_pp.bat
+@rem call tests\_set_pp.bat
+@rem echo PPPATH: %PPPATH%
+
+@set PPPATH_MSVC2019_X86=%~dp0\.out\msvc2019\x86
+@set PPPATH_MSVC2019_X64=%~dp0\.out\msvc2019\x64
+@set PPPATH_MSVC2022_X86=%~dp0\.out\msvc2022\x86
+@set PPPATH_MSVC2022_X64=%~dp0\.out\msvc2022\x64
+
 @copy /Y _distr_conf\conf\umba-md-pp.options    _distr_conf\conf\umba-md-pp-view.options
 @copy /Y _distr_conf\conf\umba-md-pp.options    _distr_conf\conf\umba-md-pp-view-console.options
-xcopy /Y /S /E /I /F /R _distr_conf\conf\*  %PPPATH%\..\conf
+
+@rem xcopy /Y /S /E /I /F /R _distr_conf\conf\*  %PPPATH%\..\conf
+
+@xcopy /Y /S /E /I /F /R _distr_conf\conf\*  %PPPATH_MSVC2019_X86%\conf
+@xcopy /Y /S /E /I /F /R _distr_conf\conf\*  %PPPATH_MSVC2019_X64%\conf
+@xcopy /Y /S /E /I /F /R _distr_conf\conf\*  %PPPATH_MSVC2022_X86%\conf
+@xcopy /Y /S /E /I /F /R _distr_conf\conf\*  %PPPATH_MSVC2022_X64%\conf
---
+++ deploy_local.bat
@@ -0,0 +1,23 @@
+@rem %UMBA_TOOLS% - eg F:\umba-tools
+
+@if "%UMBA_TOOLS%"=="" goto UMBA_TOOLS_VAR_NOT_SET
+@goto UMBA_TOOLS_VAR_IS_SET
+
+:UMBA_TOOLS_VAR_NOT_SET
+@echo UMBA_TOOLS environmetnt variable is not set
+@exit /B 1
+
+:UMBA_TOOLS_VAR_IS_SET
+
+
+@if not exist %UMBA_TOOLS%\bin    mkdir %UMBA_TOOLS%\bin
+@if not exist %UMBA_TOOLS%\conf   mkdir %UMBA_TOOLS%\conf
+
+@copy /Y .out\msvc2019\x64\Release\umba-md-pp.exe         %UMBA_TOOLS%\bin\
+@copy /Y .out\msvc2019\x64\Release\umba-md-pp-view.exe    %UMBA_TOOLS%\bin\
+
+@xcopy /Y /S /E /I /F /R _distr_conf\conf\*                  %UMBA_TOOLS%\conf
+
+%UMBA_TOOLS%\bin\umba-md-pp-view.exe --register-view-handler
+
+@rem umba-brief-scanner --help > help.txt
