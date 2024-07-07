f2af95e1511f132005657dbbe0f2efa47ef2d535
al-martyn1 <amart@mail.ru>
2024-07-07 16:23:09 +0300

    Пофиксил батник обновления

--- _update_libs_modules_from_remote.bat
+++ _update_libs_modules_from_remote.bat
@@ -1,2 +1,2 @@
-@if exist _update_modules_from_remote.bat @call _update_modules_from_remote.bat
+@if exist %~dp0\_update_modules_from_remote.bat @call %~dp0\_update_modules_from_remote.bat
 @if exist %~dp0\_libs\update_libs.bat @call %~dp0\_libs\update_libs.bat
