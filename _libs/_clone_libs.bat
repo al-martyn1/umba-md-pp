5baba2721baa034e2cb54a1d12f75172121f4876
al-martyn1 <amart@mail.ru>
2024-07-08 01:11:20 +0300

    Теперь при клонировании либ извлекаются подмодули

--- _libs/_clone_libs.bat
+++ _libs/_clone_libs.bat
@@ -24,13 +24,13 @@ goto DO_CLONE
 set PREFIX=git@%BASE%:%ACC%
 
 :DO_CLONE
-git clone %PREFIX%/umba_mm_mod_encodings.git   %~dp0\encoding
-git clone %PREFIX%/marty_cpp.git               %~dp0\marty_cpp
-git clone %PREFIX%/marty_tr.git                %~dp0\marty_tr
-git clone %PREFIX%/marty_utf.git               %~dp0\marty_utf
-git clone %PREFIX%/marty_yaml_toml_json.git    %~dp0\marty_yaml_toml_json
-git clone %PREFIX%/forks-nlohmann-json.git     %~dp0\nlohmann
-git clone %PREFIX%/umba_mm_mod_sfmt.git        %~dp0\sfmt
-git clone %PREFIX%/umba_mm_mod_umba.git        %~dp0\umba
-git clone %PREFIX%/forks-jbeder-yaml-cpp.git   %~dp0\yaml-cpp
+git clone --recurse-submodules %PREFIX%/umba_mm_mod_encodings.git   %~dp0\encoding
+git clone --recurse-submodules %PREFIX%/marty_cpp.git               %~dp0\marty_cpp
+git clone --recurse-submodules %PREFIX%/marty_tr.git                %~dp0\marty_tr
+git clone --recurse-submodules %PREFIX%/marty_utf.git               %~dp0\marty_utf
+git clone --recurse-submodules %PREFIX%/marty_yaml_toml_json.git    %~dp0\marty_yaml_toml_json
+git clone --recurse-submodules %PREFIX%/forks-nlohmann-json.git     %~dp0\nlohmann
+git clone --recurse-submodules %PREFIX%/umba_mm_mod_sfmt.git        %~dp0\sfmt
+git clone --recurse-submodules %PREFIX%/umba_mm_mod_umba.git        %~dp0\umba
+git clone --recurse-submodules %PREFIX%/forks-jbeder-yaml-cpp.git   %~dp0\yaml-cpp
 
