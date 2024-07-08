22bf0c5c71eb5b6c267b1545cc8f61d478de2427
Alexandr Martynov <mag@unicont.com>
2024-07-08 19:57:47 +0300

    Добавил поиск каталога OUT (PPROOTPATH) в скрипт setup_out_pp_root_paths.bat Обновил deploy_local.bat

--- deploy_local.bat
+++ deploy_local.bat
@@ -9,13 +9,20 @@
 
 :UMBA_TOOLS_VAR_IS_SET
 
+@call %~dp0\.bat\setup_out_pp_root_paths.bat
 @call %~dp0\update_md-pp-view-conf-options.bat
 
 @if not exist %UMBA_TOOLS%\bin    mkdir %UMBA_TOOLS%\bin
 @if not exist %UMBA_TOOLS%\conf   mkdir %UMBA_TOOLS%\conf
 
-copy /Y %~dp0\.out\msvc2019\x64\Release\umba-md-pp.exe         %UMBA_TOOLS%\bin\
-copy /Y %~dp0\.out\msvc2019\x64\Release\umba-md-pp-view.exe    %UMBA_TOOLS%\bin\
+@IF "%PPROOTPATH%"=="" @(
+    @echo PPROOTPATH not found
+    exit /B 1
+)
+
+
+copy /Y "%PPROOTPATH%\Release\umba-md-pp.exe"         %UMBA_TOOLS%\bin\
+copy /Y "%PPROOTPATH%\Release\umba-md-pp-view.exe"    %UMBA_TOOLS%\bin\
 
 @xcopy /Y /S /E /I /F /R %~dp0\_distr_conf\conf\*               %UMBA_TOOLS%\conf
 
