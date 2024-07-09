dee4b01b324abf24a3873647b6c621839184d9b9
al-martyn1 <amart@mail.ru>
2024-07-09 09:46:20 +0300

    Установил нормальный путь для пакетного режима для отладки

--- src/umba-md-pp/umba-md-pp.cpp
+++ src/umba-md-pp/umba-md-pp.cpp
@@ -232,7 +232,7 @@ int main(int argc, char* argv[])
         //argsParser.args.push_back("--batch-scan="+rootPath);
         //argsParser.args.push_back("--batch-scan="+rootPath+"doc");
         //argsParser.args.push_back("--batch-scan-recurse="+rootPath+"tests");
-        argsParser.args.push_back("--batch-scan-recurse=.");
+        argsParser.args.push_back("--batch-scan-recurse="+rootPath+".");
         
         argsParser.args.push_back("--batch-page-index-file=pages.md");
         argsParser.args.push_back("--batch-split-page-index-file");
