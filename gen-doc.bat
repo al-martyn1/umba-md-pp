b25c6d66b7c55b660eec84c878f0ead17db31c9b
Alexandr Martynov <mag@unicont.com>
2024-07-15 17:35:19 +0300

    Записал рецептик про bat файлы - удаление слеша в конце

--- gen-doc.bat
+++ gen-doc.bat
@@ -2,8 +2,16 @@
 @set EXCLUDE_DIRS=--batch-exclude-dir=_libs,libs,_lib,lib,tests,test,rc,_generators,_distr_conf,src,.msvc2019,boost,icons
 @set EXCLUDE=%EXCLUDE_DIRS% --batch-exclude-files=*upper_inc.md*
 @set COMMON=--overwrite --copy-images
+@set OPTIONS= %COMMON% %VARS% %EXCLUDE% --batch-scan-recurse=.
 
-umba-md-pp %COMMON% %BATCH_ROOT% %VARS% %EXCLUDE% --batch-scan-recurse=.
+@rem https://stackoverflow.com/questions/3160058/how-to-get-the-path-of-a-batch-script-without-the-trailing-backslash-in-a-single
+@rem Для генерации в каталог выше уровнем
+@rem set WIKI_DIR=%~dp0
+@rem set WIKI_DIR=%WIKI_DIR:~0,-1%
+@rem set WIKI_DIR=%WIKI_DIR%.wiki
+@rem @if exist %WIKI_DIR% umba-md-pp %OPTIONS% --batch-output-path=%WIKI_DIR%
+
+umba-md-pp %OPTIONS%
 
 @rem C:\work\temp\mdpp-test\ 
 
