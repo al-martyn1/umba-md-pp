423f14e30e4d454d4996cdfd4158f96ffa33109f
Alexandr Martynov <mag@unicont.com>
2024-07-29 21:24:51 +0300

    Перегенерил доку, присунул автоматом сгенерённый граф тестового trie.

--- doc/trie.bat
+++ doc/trie.bat
@@ -2,4 +2,6 @@ dot -Tsvg -Gdpi=300 -o trie.svg trie.dot
 dot -Tpng -Gdpi=300 -o trie.png trie.dot
 dot -Tsvg -Gdpi=300 -o trie_flat.svg trie_flat.dot
 dot -Tpng -Gdpi=300 -o trie_flat.png trie_flat.dot
+dot -Tsvg -Gdpi=300 -o trie_flat2.svg trie_flat2.dot
+dot -Tpng -Gdpi=300 -o trie_flat2.png trie_flat2.dot
 
