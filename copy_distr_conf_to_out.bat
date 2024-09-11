2811b65df4aaa38186a7ce8c493975eefca44f42
al-martyn1 <amart@mail.ru>
2024-09-12 00:58:42 +0300

    Шляпа

--- copy_distr_conf_to_out.bat
+++ copy_distr_conf_to_out.bat
@@ -1,8 +1,11 @@
 @call "%~dp0\update_md-pp-view-conf-options.bat"
 @call "%~dp0\.bat\setup_out_root_paths.bat"
 
+@echo OUTROOTPATHS=%OUTROOTPATHS%
+
 @FOR %%i IN (%OUTROOTPATHS%) DO @(
         @IF EXIST "%%~i\" @(
+            echo Coying "%~dp0\_distr_conf\conf\*" to "%%~i\\conf"
             xcopy /Y /S /E /I /F /R "%~dp0\_distr_conf\conf\*"  "%%~i\\conf"
 )
 )
