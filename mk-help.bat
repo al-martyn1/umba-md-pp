a0bb41a5ac03cdcb69d4e40e5887aa0d8122cf86
al-martyn1 <amart@mail.ru>
2024-09-15 19:55:32 +0300

    Добавил опции командной строки

--- mk-help.bat
+++ mk-help.bat
@@ -1,8 +1,9 @@
-@call %~dp0\tests\_setup.bat
-%PP%         --help > umba-md-pp-help.txt
-%PP% --md    --help > umba-md-pp-help.md
-%PP% --bash  --help > umba-md-pp-help.bash
-%PP% --clink --help > umba-md-pp-help.clink
+@call %~dp0.bat\setup_out_root_paths.bat
+
+@rem %OUTROOTPATH%\Debug\umba-md-pp         --help > umba-md-pp-help.txt
+%OUTROOTPATH%\Debug\umba-md-pp --md    --help > doc\umba-md-pp-help.md_
+@rem %OUTROOTPATH%\Debug\umba-md-pp --bash  --help > umba-md-pp-help.bash
+@rem %OUTROOTPATH%\Debug\umba-md-pp --clink --help > umba-md-pp-help.clink
 
 
 
