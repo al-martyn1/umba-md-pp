b0483c2a8c9cef976bd351ef53d1b722bb36f4e5
Alexandr Martynov <mag@unicont.com>
2024-09-16 15:34:29 +0300

    Убрал удаление пробелов и фикс табов из перкоммита

--- _precommit.bat
+++ _precommit.bat
@@ -1,4 +1,4 @@
 @set UMBA_MDPP_EXTRA_OPTIONS="--batch-page-index-file=%~dp0doc\pages_index.md"
 @call "%~dp0mk-help-doc.bat"
-@call "%~dp0.bat\fix_tabs.bat"
+@rem call "%~dp0.bat\fix_tabs.bat"
 @call "%~dp0.bat\gen-doc.bat"
