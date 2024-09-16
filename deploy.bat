83e4db7b1e0f44cfd5b60d9b54349c5d1d10db02
al-martyn1 <amart@mail.ru>
2024-09-17 02:50:23 +0300

    Подшаманил деплой. set_sln_exes.bat - тут задаётся список EXE, по этому списку можно также вызвать эти EXE и сгенерить MD-файлы с описанием опций

--- deploy.bat
+++ deploy.bat
@@ -1,21 +1,23 @@
-@if exist "%~dp0\set_sln.bat"    @call "%~dp0\set_sln.bat"
 @call "%~dp0\update_md-pp-view-conf-options.bat"
+@rem ---
+@if exist "%~dp0\set_sln.bat"  @call "%~dp0\set_sln.bat"
+@if exist "%~dp0\set_sln_exes.bat"  @call "%~dp0\set_sln_exes.bat"
+@if "%DEPLOY_BINS%"=="" @set DEPLOY_BINS=%SLN%
+@rem Add extra bins here
+@set DEPLOY_BINS=%DEPLOY_BINS% 
 @rem
-@if "%DEPLOY_CONFIG%"=="" @set DEPLOY_CONFIG=Release
 @if "%DEPLOY_ROOT%"==""   @set DEPLOY_ROOT=%UMBA_TOOLS%
 @rem
 @if "%DEPLOY_ROOT%"=="" goto DEPLOY_ROOT_NOT_SET
 @goto DEPLOY_ROOT_IS_SET
 :DEPLOY_ROOT_NOT_SET
-@echo DEPLOY_ROOT environmetnt variable is not set, UMBA_TOOLS not set too
+@echo DEPLOY_ROOT environment variable is not set, UMBA_TOOLS not set too
 @exit /B 1
 :DEPLOY_ROOT_IS_SET
 @rem
-@set DEPLOY_BINS=%SLN% umba-md-pp-view
-@rem
 @call "%~dp0.bat\deploy_impl.bat"
 @rem
-@rem
+@rem Add extra deploy code hre
 @if "%UMBA_TOOLS%"=="" goto DONT_REG_HANDLER
 %UMBA_TOOLS%\bin\umba-md-pp-view.exe --register-view-handler
 :DONT_REG_HANDLER
