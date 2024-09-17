dc12bb98c451236d8f5c7ad51814d52d98f2118b
Alexandr Martynov <mag@unicont.com>
2024-09-17 15:46:28 +0300

    Обновил название приложухи. Теперь для вьювера используется отдельное название

--- src/umba-md-pp/app_ver_config.h
+++ src/umba-md-pp/app_ver_config.h
@@ -1,4 +1,8 @@
-std::string appFullName   = "Umba Markdown Preprocessor";
+#if !defined(UMBA_MD_PP_VIEW)
+    std::string appFullName   = "Umba Markdown Preprocessor";
+#else
+    std::string appFullName   = "Umba MDPP Markdown Viewer";
+#endif
 std::string appVersion    = "1.0";
 std::string appCommitHash;  //UNDONE
 std::string appBuildDate  = __DATE__;
@@ -7,9 +11,16 @@ std::string appBuildTime  = __TIME__;
 const char *appHomeUrl    = "https://github.com/al-martyn1/umba-md-pp";
 const char *appistrPath   = "";
 
-#if defined(WIN32) || defined(_WIN32)
-    const char *appSubPath    = "bin/umba-md-pp.exe";
+#if !defined(UMBA_MD_PP_VIEW)
+    #if defined(WIN32) || defined(_WIN32)
+        const char *appSubPath    = "bin/umba-md-pp.exe";
+    #else
+        const char *appSubPath    = "bin/umba-md-pp";
+    #endif
 #else
-    const char *appSubPath    = "bin/umba-md-pp";
+    #if defined(WIN32) || defined(_WIN32)
+        const char *appSubPath    = "bin/umba-md-pp-view.exe";
+    #else
+        const char *appSubPath    = "bin/umba-md-pp-view";
+    #endif
 #endif
-
