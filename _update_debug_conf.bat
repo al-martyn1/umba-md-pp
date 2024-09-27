d495da815adcc7d74d8a06838017f65381099f05
Alexandr Martynov <mag@unicont.com>
2024-09-27 20:04:46 +0300

    Удалил лишние конфиги от view утилиты

--- _update_debug_conf.bat
+++ _update_debug_conf.bat
@@ -1,4 +1,2 @@
-@rem Copy MDPP configs to MDPP View configs
-@call "%~dp0\update_md-pp-view-conf-options.bat"
 @rem Update conf's for debugging
 @call "%~dp0\.bat\update_debug_conf.bat"
--- update_md-pp-view-conf-options.bat
+++
@@ -1,2 +0,0 @@
-@copy /Y %~dp0\conf\umba-md-pp.options    %~dp0\conf\umba-md-pp-view.options
-@copy /Y %~dp0\conf\umba-md-pp.options    %~dp0\conf\umba-md-pp-view-console.options
